dd5a809ff0d54d7aad8c721b07a041be5ca9253d
Aaron Lu <aaron_lu1@yahoo.com>
2020-01-14 15:23:17 +0800

    Updates

---
+++ precache-manifest.33f2b890c110756d50360b6264423ca2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0162eef03af3eb658ba5e23a50dff81d",
+    "url": "/react-songs/index.html"
+  },
+  {
+    "revision": "25e1e42a15f69e32f3be",
+    "url": "/react-songs/static/js/2.79aaac03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-songs/static/js/2.79aaac03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7208c84803551211417",
+    "url": "/react-songs/static/js/main.036108ba.chunk.js"
+  },
+  {
+    "revision": "0c426d9ce5c4f330f4ef",
+    "url": "/react-songs/static/js/runtime-main.90f7588e.js"
+  }
+]);
